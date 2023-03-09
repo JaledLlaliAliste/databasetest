45e7c23758879046995d4493c60f4226b657a5ab
JaledLlaliAliste <102748739+JaledLlaliAliste@users.noreply.github.com>
2023-03-09 23:31:51 +0100

    create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// Replace the placeholders with your MySQL database information
+$servername = "10.96.173.180";
+$username = "root";
+$password = "root";
+$dbname = "mysql";
+
+// Create a connection to the MySQL database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    // If there's an error, return an error message
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    // If the connection is successful, return a success message
+    echo "Connection successful!";
+}
+?>
